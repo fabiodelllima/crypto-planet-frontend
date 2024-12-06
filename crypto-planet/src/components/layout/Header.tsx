4966300ff821d24c2d891bb74055796e44446489
fabiodelllima <f.f.fabiodelima@gmail.com>
2024-12-06 08:39:51 -0300

    feat: Criação de toggle no Header para Mobile

--- crypto-planet/src/components/layout/Header.tsx
+++ crypto-planet/src/components/layout/Header.tsx
@@ -1,6 +1,13 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   const navigation = [
     { name: "Market", href: "/market" },
     { name: "Watchlist", href: "/error" },
@@ -14,38 +21,42 @@ const Header = () => {
     <header className="flex flex-col gap-6 p-8 border-b-2 border-black">
       <div className="flex justify-between items-center">
         <h1>Crypto Planet</h1>
-        <button>X</button>
+        <button onClick={toggleMenu}>{isMenuOpen ? "X" : "≡"}</button>
       </div>
 
-      <nav>
-        <ul className="flex flex-col gap-0.5">
-          {navigation.map((item) => (
-            <li key={item.name}>
-              <Link to={item.href}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      {isMenuOpen && (
+        <nav>
+          <ul className="flex flex-col gap-0.5">
+            {navigation.map((item) => (
+              <li key={item.name}>
+                <Link to={item.href}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
 
-      <div className="flex flex-col gap-6">
-        <select defaultValue="english-usd" className="px-4 py-2 rounded-lg">
-          <option value="english-usd">English | USD</option>
-        </select>
-        <div className="flex flex-col gap-2">
-          <button
-            onClick={() => navigate("/login")}
-            className="py-2 border-2 border-black bg-black text-white rounded-lg hover:bg-white hover:text-black"
-          >
-            Sign in
-          </button>
-          <button
-            onClick={() => navigate("/register")}
-            className="py-2 border-2 border-black bg-white text-black rounded-lg hover:bg-black hover:text-white"
-          >
-            Register
-          </button>
+      {isMenuOpen && (
+        <div className="flex flex-col gap-6">
+          <select defaultValue="english-usd" className="px-4 py-2 rounded-lg">
+            <option value="english-usd">English | USD</option>
+          </select>
+          <div className="flex flex-col gap-2">
+            <button
+              onClick={() => navigate("/login")}
+              className="py-2 border-2 border-black bg-black text-white rounded-lg hover:bg-white hover:text-black"
+            >
+              Sign in
+            </button>
+            <button
+              onClick={() => navigate("/register")}
+              className="py-2 border-2 border-black bg-white text-black rounded-lg hover:bg-black hover:text-white"
+            >
+              Register
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </header>
   );
 };
