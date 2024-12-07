998d5a73c79f1328fca77a3d6759db5b7ebe2a8f
fabiodelllima <f.f.fabiodelima@gmail.com>
2024-12-07 11:29:52 -0300

    fix: Correção em cores no componente Select

--- crypto-planet/src/components/common/Select.tsx
+++ crypto-planet/src/components/common/Select.tsx
@@ -13,10 +13,10 @@ const Select = ({ options, label, ...props }: SelectProps) => {
     <div className="flex flex-col gap-2">
       {label && <label className="text-textSecondary text-sm">{label}</label>}
       <select
-        className="px-4 py-2 bg-[#111111] border border-gray-700 rounded-lg text-gray-400 
+        className="px-4 py-2 bg-[#111111] border border-gray-700 text-gray-400 rounded-lg 
           appearance-none cursor-pointer hover:border-gray-600 transition-colors
           focus:outline-none focus:ring-1 focus:ring-gray-600
-          bg-[url('/icons/chevron-down.svg')] bg-no-repeat bg-right-1 pr-10"
+          bg-[url('/icons/chevron-down.svg')] bg-no-repeat bg-right-4 pr-10"
         {...props}
       >
         {options.map((option) => (
