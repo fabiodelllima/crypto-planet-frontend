17dd6ee6f2977a68e994c14f2370c4387b4074eb
fabiodelllima <f.f.fabiodelima@gmail.com>
2024-12-07 11:29:41 -0300

    fix: Correção em cores no componente Button

--- crypto-planet/src/components/common/Button.tsx
+++ crypto-planet/src/components/common/Button.tsx
@@ -18,18 +18,16 @@ const Button = ({
         px-4 py-2 rounded font-medium transition
         ${
           variant === "primary"
-            ? `bg-blueAccent text-white hover:bg-opacity-80 ${
+            ? `bg-blueAccent text-textPrimary hover:bg-opacity-80 ${
                 disabled ? "opacity-50 cursor-not-allowed" : ""
               }`
-            : `border border-gray-500 text-gray-200 hover:bg-gray-700 ${
+            : `border border-borderGray text-textPrimary hover:bg-container ${
                 disabled ? "opacity-50 cursor-not-allowed" : ""
               }`
         }
         ${className}
       `}
-    >
-      {props.children}
-    </button>
+    />
   );
 };
 
