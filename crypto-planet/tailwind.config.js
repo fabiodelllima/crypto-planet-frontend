55362c85cdbebdd71b234072d96858f9a5457af1
fabiodelllima <f.f.fabiodelima@gmail.com>
2024-12-07 09:33:37 -0300

    feat: Definição de cores

--- crypto-planet/tailwind.config.js
+++ crypto-planet/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueAccent: "#4F46E5",
+        background: "#121318",
+        container: "#1A1C20",
+        textPrimary: "#FFFFFF",
+        textSecondary: "#A1A1A6",
+        inputBackground: "#1A1C20",
+        borderGray: "#A1A1A6",
+        positive: "#00FF85",
+        borderGray: "#2A2C30",
+        textGray: "#A1A1A6",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
