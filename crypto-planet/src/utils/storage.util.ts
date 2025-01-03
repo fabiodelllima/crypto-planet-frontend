6c3b543a1f8237d68e2264c89efc654238c21732
fabiodelllima <f.f.fabiodelima@gmail.com>
2025-01-03 20:02:18 -0300

    refactor: Melhorias na lógica de gerenciamento de usuários e transações

--- crypto-planet/src/utils/storage.util.ts
+++ crypto-planet/src/utils/storage.util.ts
@@ -1,30 +1,63 @@
+import { ADMIN_CREDENTIALS } from "../constants/admin";
 import { IUser } from "../interfaces/auth.interface";
+import { IPortfolioTransaction } from "../interfaces/portfolio.interfaces";
 
-const USERS_KEY = "@crypto-planet:users";
-const CURRENT_USER_KEY = "@crypto-planet:current-user";
+const USERS_KEY = "users";
+const CURRENT_USER_KEY = "currentUser";
 
-export const StorageUtils = {
-  getUsers: (): IUser[] => {
-    const users = localStorage.getItem(USERS_KEY);
-    return users ? JSON.parse(users) : [];
-  },
+export function getCurrentUser(): IUser | null {
+  const userStr = localStorage.getItem(CURRENT_USER_KEY);
+  return userStr ? JSON.parse(userStr) : null;
+}
 
-  saveUser: (user: IUser) => {
-    const users = StorageUtils.getUsers();
-    users.push(user);
+export function getUsers(): IUser[] {
+  const usersStr = localStorage.getItem(USERS_KEY);
+  return usersStr ? JSON.parse(usersStr) : [];
+}
+
+export function saveUser(user: IUser): void {
+  if (user.email === ADMIN_CREDENTIALS.email) {
+    throw new Error("This email is not available");
+  }
+
+  const users = getUsers();
+
+  const newUser = {
+    ...user,
+    portfolio: {
+      total: 0,
+      totalDeposited: 0,
+      totalWithdrawn: 0,
+      lastUpdate: new Date().toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "2-digit",
+      }),
+      transactions: [],
+    },
+  };
+
+  users.push(newUser);
+  localStorage.setItem(USERS_KEY, JSON.stringify(users));
+}
+
+export function setCurrentUser(user: IUser): void {
+  localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(user));
+}
+
+export function updateUserTransactions(
+  userId: string,
+  transactions: IPortfolioTransaction[]
+): void {
+  const users = getUsers();
+  const userIndex = users.findIndex((user) => user.email === userId);
+
+  if (userIndex >= 0) {
+    users[userIndex].portfolio = {
+      ...users[userIndex].portfolio!,
+      transactions,
+    };
+
     localStorage.setItem(USERS_KEY, JSON.stringify(users));
-  },
-
-  getCurrentUser: (): IUser | null => {
-    const user = localStorage.getItem(CURRENT_USER_KEY);
-    return user ? JSON.parse(user) : null;
-  },
-
-  setCurrentUser: (user: IUser | null) => {
-    if (user) {
-      localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(user));
-    } else {
-      localStorage.removeItem(CURRENT_USER_KEY);
-    }
-  },
-};
+  }
+}
