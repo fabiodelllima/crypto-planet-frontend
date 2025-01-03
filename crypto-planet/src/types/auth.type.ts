6d95f59907e97ce8e11ffa22cbbdaaf34790f589
fabiodelllima <f.f.fabiodelima@gmail.com>
2025-01-03 19:50:08 -0300

    feat: Adc isAdmin em tipagem de AuthContextData

--- crypto-planet/src/types/auth.type.ts
+++ crypto-planet/src/types/auth.type.ts
@@ -3,6 +3,7 @@ import { IUser } from "../interfaces/auth.interface";
 export type AuthContextData = {
   user: IUser | null;
   isAuthenticated: boolean;
+  isAdmin: boolean;
   login: (email: string, password: string) => Promise<void>;
   register: (userData: Omit<IUser, "id">) => Promise<void>;
 };
