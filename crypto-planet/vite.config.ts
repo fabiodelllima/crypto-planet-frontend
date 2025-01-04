6d5c2f31882f7559c485c8cb9e0493a9eff243fb
fabiodelllima <f.f.fabiodelima@gmail.com>
2025-01-04 12:59:53 -0300

    chore: Alteração do diretório de saída do stats.html para build/

--- crypto-planet/vite.config.ts
+++ crypto-planet/vite.config.ts
@@ -1,12 +1,13 @@
+import react from "@vitejs/plugin-react";
+
 import { defineConfig } from "vite";
 import { visualizer } from "rollup-plugin-visualizer";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     react(),
     visualizer({
-      filename: "stats.html",
+      filename: "./build/stats.html",
       open: true,
       gzipSize: true,
       brotliSize: true,
