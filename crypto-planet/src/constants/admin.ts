b7d4c1326803db826caa5704efcdd3aff5b9d323
fabiodelllima <f.f.fabiodelima@gmail.com>
2025-01-04 07:20:05 -0300

    fix: Correção nos dados que serão renderizados no Admin

--- crypto-planet/src/constants/admin.ts
+++ crypto-planet/src/constants/admin.ts
@@ -1,4 +1,8 @@
 import { IUser } from "../interfaces/auth.interface";
+import {
+  portfolioData,
+  transactionsData,
+} from "../pages/Portfolio/Data/PortfolioData";
 
 export const ADMIN_CREDENTIALS: IUser = {
   email: "admin@email.com",
@@ -6,32 +10,10 @@ export const ADMIN_CREDENTIALS: IUser = {
   password: "admin",
   isAdmin: true,
   portfolio: {
-    total: 100000,
-    totalDeposited: 75000,
-    totalWithdrawn: 25000,
-    lastUpdate: "Jan 03, 2024",
-    transactions: [
-      {
-        id: "1",
-        action: "Deposited",
-        amount: 50000,
-        date: "Jan 01, 2024",
-        status: "Succesful",
-      },
-      {
-        id: "2",
-        action: "Deposited",
-        amount: 25000,
-        date: "Jan 02, 2024",
-        status: "Succesful",
-      },
-      {
-        id: "3",
-        action: "Withdrawn",
-        amount: 25000,
-        date: "Jan 03, 2024",
-        status: "Succesful",
-      },
-    ],
+    total: portfolioData.total,
+    totalDeposited: portfolioData.totalDeposited,
+    totalWithdrawn: portfolioData.totalWithdrawn,
+    lastUpdate: portfolioData.lastUpdate,
+    transactions: transactionsData,
   },
 };
